e29d8466a4a0503c8f045d9ce9a2bfb9591ade58
Vincent Regnard de Lagny <vincent.regnard.de.lagny@gmail.com>
2020-04-30 15:52:10 +0200

    Part1.13 and 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,15 +3,42 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(props.pointsInit)
+  console.log(points)
 
-  const clickHandler = () => {
+  const nextClickHandler = () => {
     setSelected(Math.floor(Math.random()*anecdotes.length))
   }
-  
+
+  const voteClickHandler = () => {
+    console.log(selected)
+    const copy = [...points]
+    copy[selected] += 1
+    console.log(copy)
+    setPoints(copy)
+  }
+
+  const findIndexOfMax = (array) => {
+    let candidate = array[0]
+    let index = 0 
+    for(let i = 1; i < array.length; i++) {
+      if (array[i] > candidate) {
+        candidate = array[i]
+        index = i
+      }
+    }
+    return index
+  }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <div>{props.anecdotes[selected]}</div>
-      <button onClick={clickHandler}>next anecdote</button>
+      <button onClick={nextClickHandler}>next anecdote</button>
+      <button onClick={voteClickHandler}>vote</button>
+      <h2>Anecdote with most votes</h2>
+      <div>{props.anecdotes[findIndexOfMax(points)]}</div>
+      <div>has {points[findIndexOfMax(points)]} votes</div>
     </div>
   )
 }
@@ -25,7 +52,10 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+const pointsInit = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0)
+console.log(pointsInit)
+
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anecdotes={anecdotes} pointsInit={pointsInit}/>,
   document.getElementById('root')
 )
