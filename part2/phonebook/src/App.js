03ae71e74165d81feb623c2e2ee20d0261d17b97
Regcent <vincent.regnard.de.lagny@gmail.com>
2020-05-01 14:17:57 +0200

    2.9 : add name filtering, case insensitive

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,14 +3,23 @@ import Contact from './components/Contact'
 
 const App = () => {
     const [ persons, setPersons ] = useState([
-      { name: 'Arto Hellas' , number: '000000'}
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ]) 
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
+    const [ newFilter, setNewFilter ] = useState('')
   
     const handleNameChange = (event) => setNewName(event.target.value)
 
     const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+    const handleFilterChange = (event) => {
+        setNewFilter(event.target.value)
+
+    }
 
     const addContact = (event) => {
         event.preventDefault()
@@ -32,22 +41,29 @@ const App = () => {
     return (
       <div>
         <h2>Phonebook</h2>
+        <div>
+            filter shown with: <input
+                value={newFilter}
+                onChange={handleFilterChange}/>
+        </div>
+        <h2>Add a new</h2>
         <form onSubmit={addContact}>
           <div>
             name: <input 
-            value={newName}
-            onChange={handleNameChange}/>
+                value={newName}
+                onChange={handleNameChange}/>
           </div>
           <div>
               number: <input 
-              value={newNumber}
-              onChange={handleNumberChange}/></div>
+                value={newNumber}
+                onChange={handleNumberChange}/></div>
           <div>
             <button type="submit">add</button>
           </div>
         </form>
         <h2>Numbers</h2>
-        {persons.map((person) => <Contact key={person.name} number={person.number} name={person.name}/>)}
+        {persons.filter((person) => (person.name.toLowerCase().startsWith(newFilter.toLowerCase())))
+            .map((person) => <Contact key={person.name} number={person.number} name={person.name}/>)}
       </div>
     )
   }
