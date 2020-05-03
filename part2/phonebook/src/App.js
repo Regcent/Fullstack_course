3bae4a478c6bbd2083158dc91376330fc62fae55
Regcent <vincent.regnard.de.lagny@gmail.com>
2020-05-03 16:00:02 +0200

    Exercises 2.19 and 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
+import Error from './components/Error'
 import personsService from './services/contacts.js'
+import './index.css'
 
 const App = () => {
     const [ persons, setPersons ] = useState([]) 
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ newFilter, setNewFilter ] = useState('')
+    const [ notification, setNotification ] = useState(null)
+    const [ errorMessage, setErrorMessage ] = useState(null)
 
     useEffect(() => {
       personsService
@@ -29,23 +34,47 @@ const App = () => {
               const id = persons.find((person) => person.name === newName).id
               personsService
                 .update(id, {name: newName, number: newNumber})
-                .then(updatedPerson => setPersons(persons.map((person) => person.id !== id ? person : updatedPerson)))
-            }
+                .then(updatedPerson => {
+                  setPersons(persons.map((person) => person.id !== id ? person : updatedPerson))
+                  setNotification(`${newName} was updated`)
+                  setTimeout(() => {
+                    setNotification(null)
+                  }, 3000)
+                })
+                .catch(error => {
+                  setErrorMessage(`${newName}'s information has already been deleted from server.`)
+                  setPersons(persons.filter(person => person.id !== id ? person : null))
+                  setTimeout(() => {
+                    setErrorMessage(null)
+                  }, 3000)
+            })
+          }
         }
         else {
             personsService
               .create({name: newName, number: newNumber})
-              .then(createdPerson => setPersons(persons.concat(createdPerson)))
+              .then(createdPerson => {
+                setPersons(persons.concat(createdPerson))
+                setNotification(`${newName} was added`)
+                setTimeout(() => {
+                  setNotification(null)
+                }, 3000)
+              })
             setNewName('')
             setNewNumber('')
         }
-    }
+      }
 
     const deletePersonWithId = (id) => {
       return (event) => {
-        if (window.confirm(`Delete ${persons.find((person) => person.id === id).name}?`)) {
+        const name = persons.find((person) => person.id === id).name
+        if (window.confirm(`Delete ${name}?`)) {
           personsService.deleteContact(id)
           setPersons(persons.filter(person => person.id !== id ? person : null))
+          setNotification(`${name} was deleted`)
+            setTimeout(() => {
+              setNotification(null)
+            }, 3000)
         }
       }
     }
@@ -53,6 +82,8 @@ const App = () => {
     return (
       <div>
         <h2>Phonebook</h2>
+        <Notification message={notification} />
+        <Error message={errorMessage} />
         <Filter filter={newFilter} setNewFilter={setNewFilter}/>
         <h2>Add a new</h2>
         <PersonForm newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber} addContact={addContact}/>
