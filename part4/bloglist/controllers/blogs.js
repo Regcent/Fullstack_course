e45653b56fb26fe118fa9d133b35c424cc1b4bd6
Regcent <vincent.regnard.de.lagny@gmail.com>
2020-05-23 18:18:40 +0200

    Add router (+ util routes), app working

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -7,3 +7,30 @@ blogRouter.get('/', (request, response) => {
   })
 })
 
+blogRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+blogRouter.post('/', (request, response, next) => {
+  Blog(request.body).save()
+    .then((result) => response.status(201).json(result))
+    .catch((error) => next(error))
+})
+
+blogRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = blogRouter
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
 // const http = require('http')
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -14,24 +13,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
