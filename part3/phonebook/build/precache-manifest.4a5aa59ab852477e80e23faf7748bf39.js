42df7184627141d818ab9fd0ed0a5c6e7fadd471
Regcent <vincent.regnard.de.lagny@gmail.com>
2020-05-23 09:46:06 +0200

    Recommit 3.20 with the right build, forgot to rm -rf between builds copy

--- part3/phonebook/build/precache-manifest.4a5aa59ab852477e80e23faf7748bf39.js
+++ part3/phonebook/build/precache-manifest.4a5aa59ab852477e80e23faf7748bf39.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "47d950343d2fd237cf05e8a098121edc",
+    "revision": "1b1dfbbf498da30de6f70c42364e18ba",
     "url": "/index.html"
   },
   {
-    "revision": "90062ca316d1a742d87f",
+    "revision": "d465ab8df8af7b378667",
     "url": "/static/css/main.02e76467.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90062ca316d1a742d87f",
-    "url": "/static/js/main.1907f35d.chunk.js"
+    "revision": "d465ab8df8af7b378667",
+    "url": "/static/js/main.8f4ff7f3.chunk.js"
   },
   {
     "revision": "37e42bc47805ba865915",
